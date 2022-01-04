463a6ebf1722c09f522bde648ff8c7fe0efe1cf0
Ashwin Madhu <ashwin.madhu@experionglobal.local>
2022-01-04 07:53:27 +0530

    V2 validation with tool tips

--- validation1.js
+++ validation1.js
@@ -2,6 +2,7 @@ function checkName(idEle) {
   nameObj = document.getElementById(idEle);
   nameObj.classList.remove("is-invalid");
   value = nameObj.value;
+  nameObj.setAttribute("title", "")
   console.log(value);
   if (value == "") {
     console.log("Name is a mandatory field");
@@ -9,7 +10,10 @@ function checkName(idEle) {
   let pattern = /[^a-z A-Z]/g;
   let result = value.match(pattern);
   console.log(result);
-  if (result) nameObj.classList.add("is-invalid");
+  if (result){
+    nameObj.classList.add("is-invalid");
+    nameObj.setAttribute("title", "Name Should Have Alphabets Only");
+  } 
 }
 
 function checkPhone(idEle) {
@@ -45,4 +49,31 @@ function checkDOB(idEle) {
       dateObj.classList.add("is-invalid");
     }
   
+  }
+
+
+  function checkNumber(idEle) {
+    Obj = document.getElementById(idEle);
+    Obj.classList.remove("is-invalid");
+    value = Obj.value;
+    console.log(value);
+    if (value.match(/[^0-9]/g)) {
+      Obj.classList.add("is-invalid");
+    }
+
+  }
+  
+
+  function greaterThan(idEle,compare) {
+    let Obj1 = document.getElementById(idEle);
+    Obj1.classList.remove("is-invalid");
+    let value1 = Obj1.value;
+    Obj2 = document.getElementById(compare);
+    let value2 = Obj2.value;
+    console.log(value1);
+    console.log(value2);
+    if (parseInt(value1)< parseInt(value2)) {
+      Obj1.classList.add("is-invalid");
+    }
+
   }
